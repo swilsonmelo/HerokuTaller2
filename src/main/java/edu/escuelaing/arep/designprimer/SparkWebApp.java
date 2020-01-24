6f43bd6a397d644f21bfc6e2857b76dfbfc68a5b
Willson <swilsonmelo@gmail.com>
2020-01-24 12:24:10 -0500

    no sé que estoy haciendo

---
+++ src/main/java/edu/escuelaing/arep/designprimer/SparkWebApp.java
@@ -0,0 +1,18 @@
+package edu.escuelaing.arep.designprimer;
+
+import static spark.Spark.*;
+
+public class SparkWebApp {
+
+    public static void main(String[] args) {
+        port(getPort());
+        get("/hello", (req, res) -> "Hello Heroku");
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; // returns default port if heroku-port isn't set(i.e. on localhost)
+    }
+}
